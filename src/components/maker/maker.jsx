a972d6f82e69025fab0b9d63226a71e9f817adf2
kyondoku <kyondoku@gmail.com>
2022-04-08 23:14:23 +0900

    .

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react/cjs/react.production.min";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+import styles from "./maker.module.css";
 
-const Maker = (props) => <h1>Maker</h1>;
+const Maker = ({ authService }) => {
+  const navigate = useNavigate();
+  const onLogout = () => {
+    authService.logout();
+  };
+
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      if (!user) {
+        navigate("/");
+      }
+    });
+  });
+  return (
+    <section className={styles.maker}>
+      <Header onLogout={onLogout}></Header>
+      <Footer />
+    </section>
+  );
+};
 
 export default Maker;
